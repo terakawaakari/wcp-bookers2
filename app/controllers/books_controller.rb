88955a847fc276fbd712fc20c1362fdc75c25776
terakawaakari <terakawaakari0701@gmail.com>
2021-05-04 08:35:56 +0000

    [Finish]4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,31 +9,27 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-   if @book.save
+    if @book.save
       redirect_to book_path(@book), notice:'You have created book successfully.'
-   else
+    else
       @books = Book.page(params[:page]).reverse_order
       @user_id = current_user
       render :index
-   end
-
+    end
   end
 
   def show
-    @user_find = User.find_by(params[:id])
-    #@user_find_id = @user_find.id
     @book_find = Book.find(params[:id])
     @user = current_user
-    @user_id = current_user
     @book = Book.new
   end
 
   def edit
     @book = Book.find(params[:id])
     @user_id = current_user
-     unless @book.user == current_user
-       redirect_to book_path(current_user)
-     end
+    unless @book.user == current_user
+      redirect_to books_path
+    end
   end
 
   def update
